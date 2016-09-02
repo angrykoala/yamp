2cada8b070dd317bb864285113a63586b11e7f7a
demiurgosoft <demiurgosoft@hotmail.com>
2016-09-02 23:33:57 +0100

    basic template and jshint config, close #9

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 #!/usr/bin/env node
 
+"use strict";
 
 //If using npm use -- before arguments
-var commander = require('commander');
-var md = require('markdown-it')();
-var pkginfo = require('pkginfo')(module, "version", "author", "license", "description");
-var fs = require('fs');
-var pdf = require('html-pdf');
+let commander = require('commander');
+let md = require('markdown-it')();
+require('pkginfo')(module, "version", "author", "license", "description");
+let fs = require('fs');
+let pdf = require('html-pdf');
+let ejs = require('ejs');
 
-var version = module.exports.version;
+let version = module.exports.version;
 
-var inputFile;
-var outputFile;
+let inputFile;
+let outputFile;
 commander.version(version)
     .usage("yamp [options] <file>")
     .description(module.exports.description)
@@ -23,8 +25,9 @@ commander.version(version)
     .option("-o, --output <file>", "output file")
     .parse(process.argv);
 
+let prev = '<link rel="stylesheet" href="github-markdown.css"><style>.markdown-body {box-sizing: border-box;        min-width: 200px;        max-width: 980px;        margin: 0 auto;      padding: 45px;    }</style> <article class="markdown-body">';
 
-var prev = '<link rel="stylesheet" href="github-markdown.css"><style>.markdown-body {box-sizing: border-box;        min-width: 200px;        max-width: 980px;        margin: 0 auto;      padding: 45px;    }</style> <article class="markdown-body">';
+let prev2 = '<link rel="stylesheet" href="acm-sig.css">';
 
 if (commander.output) outputFile = commander.output;
 
@@ -32,20 +35,32 @@ if (!inputFile || !outputFile) {
     return console.error("Invalid Input", "usage: yamp [options] <file>");
 }
 console.log(__dirname);
+
 fs.readFile(inputFile, 'utf8', function(err, data) {
     if (err) {
         return console.error("Error reading file\n", err);
     }
-    var html = prev + md.render(data) + "</article>";
-    fs.writeFile(outputFile+'.html', html, function(err) {
+    data = md.render(data);
+    ejs.renderFile('./templates/default.ejs', {
+        title : "My own title",
+        content : data
+    }, {}, function(err, html) {
         if (err) {
-            return console.error("Error writing file\n", err);
+            return console.error("Error parsing ejs\n", err);
         }
 
-        console.log("HTML successfully created");
-        pdf.create(html,{"base": "file://"+__dirname+"/styles/"}).toFile(outputFile + '.pdf', function(err, res) {
-            if (err) return console.error(err);
-            return console.log("PDF successfully created");
+        fs.writeFile(outputFile + '.html', html, function(err) {
+            if (err) {
+                return console.error("Error writing file\n", err);
+            }
+
+            console.log("HTML successfully created");
+            pdf.create(html, {
+                "base": "file://" + __dirname + "/styles/"
+            }).toFile(outputFile + '.pdf', function(err, res) {
+                if (err) return console.error(err);
+                return console.log("PDF successfully created");
+            });
         });
     });
 });
