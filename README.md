fcecc0de13560a8ded3583f633ced483e8a6619e
demiurgosoft <demiurgosoft@hotmail.com>
2016-09-02 20:34:37 +0100

    pdf output using html-pdf and github-markdown.css, close #2

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 yamp
 ====
 _by @angrykoala_
+
 Yet Another Markdown Parser <https://github.com/angrykoala/yamp>
 
 The aim of this package is to provide an easy-to-use toolbox for markdown related task including:
@@ -11,5 +12,7 @@ The aim of this package is to provide an easy-to-use toolbox for markdown relate
 
 ## Acknowledgements
 * [markdown-it](https://github.com/markdown-it/markdown-it) as markdown parser
+* [github-markdown.css](https://github.com/sindresorhus/github-markdown-css) as default parse style
 
 >YAMP is developed under GNU GPL-3 license by @angrykoala
+>github-markdown.css 
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var commander = require('commander');
 var md = require('markdown-it')();
 var pkginfo = require('pkginfo')(module, "version", "author", "license", "description");
 var fs = require('fs');
+var pdf = require('html-pdf');
 
 var version = module.exports.version;
 
@@ -23,21 +24,28 @@ commander.version(version)
     .parse(process.argv);
 
 
+var prev = '<link rel="stylesheet" href="github-markdown.css"><style>.markdown-body {box-sizing: border-box;        min-width: 200px;        max-width: 980px;        margin: 0 auto;      padding: 45px;    }</style> <article class="markdown-body">';
+
 if (commander.output) outputFile = commander.output;
 
 if (!inputFile || !outputFile) {
     return console.error("Invalid Input", "usage: yamp [options] <file>");
 }
-
+console.log(__dirname);
 fs.readFile(inputFile, 'utf8', function(err, data) {
     if (err) {
         return console.error("Error reading file\n", err);
     }
-    fs.writeFile(outputFile, md.render(data), function(err) {
+    var html = prev + md.render(data) + "</article>";
+    fs.writeFile(outputFile+'.html', html, function(err) {
         if (err) {
-            return console.err("Error writing file\n", err);
+            return console.error("Error writing file\n", err);
         }
 
-        return console.log("HTML successfully generate");
+        console.log("HTML successfully created");
+        pdf.create(html,{"base": "file://"+__dirname+"/styles/"}).toFile(outputFile + '.pdf', function(err, res) {
+            if (err) return console.error(err);
+            return console.log("PDF successfully created");
+        });
     });
 });
